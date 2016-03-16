04e0cc73e33c744b2422ee499b0925baeef60716
git5056 <user5056@163.com>
2016-03-16 20:51:58 +0800

    NewNetDiskDomain

---
+++ NetDiskHelper/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetDiskHelper
+{
+    public class Class1
+    {
+    }r
+}
---
+++ NetDiskService/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetDiskService
+{
+    public class Class1
+    {
+    }
+}
